2410d7f612c56a5120a15f17adf3f93f2d8c66fc
Oliver Paisley <oliverjdpaisley@gmail.com>
2022-12-31 16:29:31 -0800

    Don't want to cache

--- helper.sh
+++ helper.sh
@@ -2,7 +2,7 @@ BRANCH=${1:-agenica-dev} # take 0th argument if given, else use default
 echo $BRANCH
 
 # Build container
-devcontainer build --image-name $BRANCH --workspace-folder agencia
+devcontainer build --no-cache --image-name $BRANCH --workspace-folder agencia
 
 # Open VSCode with attached container
 devcontainer open agencia
