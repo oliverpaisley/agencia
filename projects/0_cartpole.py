779e00d043dfe69d37d7d195c13e775c5f477d9a
Oliver Paisley <oliverjdpaisley@gmail.com>
2023-09-07 17:44:18 -0700

    Move CartPole script out of agencia folder

--- projects/0_cartpole.py
+++ projects/0_cartpole.py
@@ -1,10 +1,14 @@
-from agencia.agents import Agent
-from agencia.envs import CartPole
-from agencia import infra
+from agencia.envs import CartPoleEnv
+from tqdm import tqdm
 
+cart_pole_env = CartPoleEnv()
+env = cart_pole_env.make(render_mode="human")
 
+observation, info = env.reset(seed=42)
+for _ in tqdm(range(100000)):
+   action = env.action_space.sample()  # agent policy that uses the observation and info
+   observation, reward, terminated, truncated, info = env.step(action)
 
-env = CartPole()
-# agt = Agent_Random()
-# task = create_task(agt, env)
-# task.run()
+   if terminated or truncated:
+      observation, info = env.reset()
+env.close()
