25dada8940df1279a8f9d907d5dfae3cd4c43bdc
Oliver Paisley <oliverjdpaisley@gmail.com>
2023-09-12 09:10:17 -0700

    Testing Black by messing up formatting

--- projects/0_cartpole.py
+++ projects/0_cartpole.py
@@ -6,9 +6,7 @@ env = cart_pole_env.make(render_mode="human")
 
 observation, info = env.reset(seed=42)
 for _ in tqdm(range(100000)):
-    action = (
-        env.action_space.sample()
-    )  # agent policy that uses the observation and info
+    action = (env.action_space.sample())  # agent policy that uses the observation and info
     observation, reward, terminated, truncated, info = env.step(action)
 
     if terminated or truncated:
